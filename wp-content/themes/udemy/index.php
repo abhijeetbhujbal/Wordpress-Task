7f71334a832578b91cbf7d96ed76156ad96ceb29
Abhijeet <abhijeet.bhujbal@cgparivar.com>
2022-08-24 15:23:03 +0530

    Initial commit

---
+++ wp-content/themes/udemy/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+   <?php wp_head();?>
+</head>
+<body <?php body_class('example'); ?>>
+
+<h1>Test Doc</h1>
+ 
+<?php wp_body_open();?>
+
+<?php wp_footer();?>
+</body>
+</html>
